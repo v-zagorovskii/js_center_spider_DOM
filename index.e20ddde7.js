9c061bbb0a71f505b4a856d7b15b535a9a79796e
Vlad Zahorovskyi <zagorovskii12@gmail.com>
2024-08-17 20:02:29 +0300

    make build

---
+++ index.e20ddde7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth-t.clientWidth,l=e.clientHeight-t.clientHeight;t.style.top="".concat(l/2,"px"),t.style.left="".concat(c/2,"px");
+//# sourceMappingURL=index.e20ddde7.js.map
